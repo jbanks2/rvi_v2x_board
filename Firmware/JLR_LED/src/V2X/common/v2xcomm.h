4b2a711b3886d3bed97e21a214ed27392641db9d
Jesse Banks <jbanks2@jaguarlandrover.com>
2015-11-13 10:14:36 -0800

    Added the firmware from UCSD

---
+++ Firmware/JLR_LED/src/V2X/common/v2xcomm.h
@@ -0,0 +1,16 @@
+/*
+ * v2xcomm.h
+ *
+ * Created: 10/15/2015 3:42:57 PM
+ *  Author: Kevin
+ */ 
+
+
+#ifndef V2XCOMM_H_
+#define V2XCOMM_H_
+
+#include "../../asf.h"
+
+#define ENABLE_3V3 0X01
+
+#endif /* V2XCOMM_H_ */
